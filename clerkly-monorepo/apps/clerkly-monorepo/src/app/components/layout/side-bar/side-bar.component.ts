de8db4bc8d3bf581b1a3ea81899be6e0df48887f
raf-xtgt <rafaquat9905@gmail.com>
2024-12-08 17:30:41 +0600

    Added tailwind and layout components

---
+++ clerkly-monorepo/apps/clerkly-monorepo/src/app/components/layout/side-bar/side-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-side-bar',
+  imports: [CommonModule],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css',
+})
+export class SideBarComponent {}
---
+++ clerkly-monorepo/apps/clerkly-monorepo/src/app/components/layout/top-bar/top-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-top-bar',
+  imports: [CommonModule],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css',
+})
+export class TopBarComponent {}
